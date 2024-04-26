3fc5edc85403456649b1233fb2ec5dbe06cc0415
adrian6922 <joffred.vargas.ext@redsalud.cl>
2024-04-25 23:04:38 -0400

    avances clase modulo

---
+++ organizacion_modulo.py
@@ -0,0 +1,73 @@
+# def elevar(base, exponente):
+#     """Esta función tiene como objetivo elevar una base a un exponente"""
+#     return base**exponente
+# elevar()
+
+# def elevar(base, exponente):
+#     """[summary]
+#     Args:
+#         base ([type]): [description]
+#         exponente ([type]): [description]
+#     Returns:
+#         [type]: [description]
+#     """
+#     return base**exponente
+
+# elevar()
+
+# def elevar(base, exponente):
+#     """[summary]
+#     :param base: [description]
+#     :type base: [type]
+#     :param exponente: [description]
+#     :type exponente: [type]
+#     :return: [description]
+#     :rtype: [type]
+#     """
+#     return base**exponente
+# elevar()
+
+# def elevar(base, exponente):
+#     """[summary]
+#     Arguments:
+#     base {[type]} -- [description]
+#     exponente {[type]} -- [description]
+#     Returns:
+#     [type] -- [description]
+#     """
+#     return base**exponente
+# elevar()
+
+# def elevar(base, exponente):
+#     """Esta función tiene como objetivo
+#     elevar una base a un exponente.
+#     Parameters
+#     ----------
+#     base : [float]
+#     Base de la Potencia.
+#     exponente : [float]
+#     Exponente de la Potencia
+#     Returns
+#     -------
+#     [float]
+#     Retorna el resultado de elevar base a
+#     exponente
+#     """
+#     return base**exponente
+# elevar()
+
+
+#sumar datos
+n1 = input("Ingrese un número: ")
+n2 = input("Ingrese un número: ")
+n3 = input("Ingrese un número: ")
+sumar = n1 + n2
+print(sumar)
+
+def sumar2(a,b,c):
+    return a + b + c
+sumar2()
+
+#pagina contactos, galeria, correos, etc
+saludo = "Hola Mundo"
+sumatoria_total = 2 + 3 + 5 + 6
